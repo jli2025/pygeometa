0b2d760950e460ef56f6dd37b78a1b47b49b35a4
jiarongli <jiarong.li@wur.nl>
2025-11-20 15:09:14 +0100

    last update on the old openaire api format

--- pygeometa/schemas/openaire/__init__.py
+++ pygeometa/schemas/openaire/__init__.py
@@ -133,7 +133,58 @@ class OpenAireOutputSchema(BaseOutputSchema):
             mcf['metadata']['hierarchylevel'] = instance_type_
         
         mcf['metadata']['datestamp'] = result_.get('header', {}).get('dri:dateOfCollection', {}).get('$')
-     
+        mcf['metadata']['dataseturi'] = main_instance_.get('webresource', {}).get('url', {}).get('$')
+
+        # mcf: identification
+
+        mcf['identification']['language'] = metadata_.get('language', {}).get('@classid')
+        title_ = metadata_.get('title', {})
+        if isinstance(title_, list):
+            mcf['identification']['title'] = [t.get('$') for t in title_]
+        elif isinstance(title_, dict):
+            mcf['identification']['title'] = title_.get('$')
+        description_ = metadata_.get('description', {}).get('$')
+        mcf['identification']['abstract'] = description_
+        ## edition/version
+        ## topiccategory
+
+        right_ = metadata_.get('bestaccessright', {}).get('@classname')
+        instance_right_ = main_instance_.get('accessright', {}).get('@classname')
+        if right_ is not None and right_ != 'unspecified':
+            mcf['identification']['rights'] = right_
+        elif instance_right_ is not None and instance_right_ != 'unspecified':
+            mcf['identification']['rights'] = instance_right_
+        else:
+            mcf['identification']['rights'] = 'unspecified'
+        
+        license_ = main_instance_.get('license', {}).get('$')
+        if license_:
+            mcf['identification']['license'] = {'name': license_, 'url': ''}
+        
+        ## url
+        dates_ = metadata_.get('relevantdate')
+        dates_dict = {}
+        if isinstance(dates_, list):
+            for d in dates_:
+                d_name_ = d.get('@classname')
+                d_value = d.get('$')
+                if d_name_ and d_value:
+                    dates_dict[d_name_] = d_value
+        elif isinstance(dates_, dict):
+            d_name_ = dates_.get('@classname')
+            d_value = dates_.get('$')
+            if d_name_ and d_value:
+                dates_dict[d_name_] = d_value
+        if dates_dict: 
+            mcf['identification']['dates'] = dates_dict
+        ## keywords
+
+        subjects_ = metadata_.get('subject')
+        if isinstance(subjects_, dict):
+            mcf['identification']['keywords'] = process_keywords([subjects_])
+        elif isinstance(subjects_, list):
+            mcf['identification']['keywords'] = process_keywords(subjects_)
+
         print(mcf)
         return mcf
 
@@ -183,13 +234,36 @@ def process_id_and_instance(ids: list, instances: list) -> tuple[str, object]:
                 break
     if len(instances) == 0:
         return main_id, None
+    
     # get the instance matched with the main id
     main_instance = instances[0]
     for ins in instances:
         pid = ins.get('pid', {})
-        pid_value = pid.get('$') if pid else None
-        if pid_value == main_id:
-            main_instance = ins
-            break
+        if isinstance(pid, list): # instance has multiple pid
+            pid_values = [i.get('$') for i in pid]
+            if main_id in pid_values:
+                main_instance = ins
+                break
+        elif isinstance(pid, dict): # instance has one pid
+            if pid.get('$') == main_id:
+                main_instance = ins
+                break
+        else:
+            continue
     return main_id, main_instance
     
+def process_keywords(subjects: list) -> dict:
+    """
+    convert openaire keywords to mcf keywords
+
+    group keywords by classid
+    
+    """
+    unique_kid = list(set([s.get('@classid') for s in subjects]))
+    
+    keyword_dict = {kid: {'keywords': [], 'keywords_type': ''} for kid in unique_kid} 
+    for kid in unique_kid:
+        for s in subjects:
+            if s.get('@classid') == kid:
+                keyword_dict[kid]['keywords'].append(s.get('$'))
+ 
